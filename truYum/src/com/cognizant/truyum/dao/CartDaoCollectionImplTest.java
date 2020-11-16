4b7543f49e8c55e9463e40bb857102e2674ca34e
ishubhn <imshubhn@gmail.com>
2020-11-16 18:02:20 +0530

    test

---
+++ truYum/src/com/cognizant/truyum/dao/CartDaoCollectionImplTest.java
@@ -0,0 +1,19 @@
+package com.cognizant.truyum.dao;
+
+public class CartDaoCollectionImplTest {
+	public void testAddCartItem() {
+
+	}
+
+	public void testGetAllCartItems() {
+
+	}
+
+	public void testRemoteCartItem() {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
---
+++ truYum/src/com/cognizant/truyum/dao/MenuItemDaoCollectionImplTest.java
@@ -0,0 +1,24 @@
+package com.cognizant.truyum.dao;
+
+public class MenuItemDaoCollectionImplTest {
+
+	public void testGetMenuItemListAdmin() {
+
+	}
+
+	public void testGetMenuItemListCustomer() {
+
+	}
+
+	public void testModifyMenuItem() {
+
+	}
+
+	public void testGetMenuItem() {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
