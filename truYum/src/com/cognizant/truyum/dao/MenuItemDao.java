5aa26bab4200f8f8818dd4ca51bd47e71d5bec04
ishubhn <imshubhn@gmail.com>
2020-11-16 17:52:36 +0530

    *

--- truYum/src/com/cognizant/truyum/dao/MenuItemDao.java
+++ truYum/src/com/cognizant/truyum/dao/MenuItemDao.java
@@ -5,8 +5,11 @@ import java.util.List;
 import com.cognizant.truyum.model.MenuItem;
 
 public interface MenuItemDao {
-	public List<MenuItem> getMenuItemListtAdmin();
+	public List<MenuItem> getMenuItemListAdmin();
+	
 	public List<MenuItem> getMenuItemListCustomer();
+	
 	public void modifyMenuItem(MenuItem menuItem);
-	public MenuItem getMenuItem(long menuitemId);
+	
+	public MenuItem getMenuItem(long menuItemId);
 }
--- truYum/src/com/cognizant/truyum/dao/MenuItemDaoCollectionImpl.java
+++ truYum/src/com/cognizant/truyum/dao/MenuItemDaoCollectionImpl.java
@@ -5,25 +5,10 @@ import java.util.List;
 import com.cognizant.truyum.model.MenuItem;
 
 public class MenuItemDaoCollectionImpl implements MenuItemDao {
-	private List<MenuItem> menuItemList;
+	private static List<MenuItem> menuItemList;
 	
-	
-	public List<MenuItem> getMenuItemList() {
-		return menuItemList;
-	}
-
-	public void setMenuItemList(List<MenuItem> menuItemList) {
-		this.menuItemList = menuItemList;
-	}
-
-	public MenuItemDaoCollectionImpl(List<MenuItem> menuItemList) {
-		super();
-		this.menuItemList = menuItemList;
-	}
-
-	/*METHODS V*/
 	@Override
-	public List<MenuItem> getMenuItemListtAdmin() {
+	public List<MenuItem> getMenuItemListAdmin() {
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -37,13 +22,21 @@ public class MenuItemDaoCollectionImpl implements MenuItemDao {
 	@Override
 	public void modifyMenuItem(MenuItem menuItem) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
-	public MenuItem getMenuItem(long menuitemId) {
+	public MenuItem getMenuItem(long menuItemId) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public List<MenuItem> getMenuItemList() {
+		return menuItemList;
+	}
+
+	public void setMenuItemList(List<MenuItem> menuItemList) {
+		this.menuItemList = menuItemList;
 	}
 
 }
